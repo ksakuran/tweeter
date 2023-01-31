77a922ec1fbf28f6e39d29b6f5a479d17fae4828
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-31 03:13:01 +0000

    inspect files for compass exercise

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const bodyParser    = require("body-parser");
 const app           = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+//serves static page in public file, index html?? -kn
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
