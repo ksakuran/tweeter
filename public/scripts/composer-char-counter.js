310b343074c7652657f48a250a238fd196e173e6
Kat Nishimura <katherine.nishimura@live.ca>
2023-02-01 04:20:58 +0000

    add tweet container section, style header, footer, p, add icons in footer

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
   console.log("document is ready");
-  
+  //access the textarea by id to get the value of characters by length
   $('#tweet-composer').keyup(function(event) {
     const textLength = $(this).val().length;
     const maxLength = 140;
@@ -11,6 +11,7 @@ $(document).ready(function() {
     const counter = $(this).parent().next().children().last();
     counter.html(textCount);
 
+    //changes the class of the counter so style in css applied as color red if negative
     if (textCount < 0) {
       $(counter).removeClass('counter');
       $(counter).addClass('negative-count');
