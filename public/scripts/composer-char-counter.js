5ecdab577d9402013dccfcbc32f456121fb1f4e1
Kat Nishimura <katherine.nishimura@live.ca>
2023-02-01 05:07:41 +0000

    add hover event for icons, add styling for hover class to change color

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,16 +2,18 @@
 
 $(document).ready(function() {
   console.log("document is ready");
+
   //access the textarea by id to get the value of characters by length
   $('#tweet-composer').keyup(function(event) {
     const textLength = $(this).val().length;
     const maxLength = 140;
     const textCount = maxLength - textLength;
+
     //reassign the counter element to show the amount of characters left
     const counter = $(this).parent().next().children().last();
     counter.html(textCount);
 
-    //changes the class of the counter so style in css applied as color red if negative
+    //changes the class of the counter to show color of text red if negative
     if (textCount < 0) {
       $(counter).removeClass('counter');
       $(counter).addClass('negative-count');
@@ -21,6 +23,5 @@ $(document).ready(function() {
     }
 
   });
-
 
 });
