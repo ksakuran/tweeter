4c2644744c8aa738c61442aa095844375d123ff6
Kat Nishimura <katherine.nishimura@live.ca>
2023-02-02 01:59:33 +0000

    add loadTweets function to make get request, render tweets using response, add timeago format

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,7 +19,7 @@ const createTweetElement = function(tweet) {
       ${tweet.content.text}
       </p>
     <footer class="tweet-container">
-      ${tweet.created_at}
+      ${timeago.format(tweet.created_at)}
       <span class="tweet-container">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -27,25 +27,38 @@ const createTweetElement = function(tweet) {
       </span>
     </footer>
   </article>
-  `)
+  `);
 
   return $newTweet;
 }
 
-//test driver code 
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//test driver code, fake data
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
 //       "handle": "@SirIsaac"
 //     },
-//   "content": {
+//     "content": {
 //       "text": "If I have seen further it is by standing on the shoulders of giants"
 //     },
-//   "created_at": 1461116232227
-// }
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
-//test dirver code ^^^^
+//renders tweets from database into html elements and appends to the tweet section
 const renderTweets = function (databaseArray) {
 
   databaseArray.forEach(element => {
@@ -57,35 +70,30 @@ const renderTweets = function (databaseArray) {
   });
 }
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 $(document).ready(function () {
 
-  //console.log("hello from client, here is new tweet")
-  
-  renderTweets(data)
+  const $tweetForm = $('form.new-tweet');
 
+  $tweetForm.submit(function(event) {
+    event.preventDefault();
+
+    //url encode the data from the form since the server is expecting this format
+    const data = $tweetForm.serialize();
+    
+    //create post request with data from tweet form
+    $.post('/tweets/', data, (response) => {   
+    });
+
+  });
+
+  //make a get request and recieve the array of tweets as JSON
+  const loadTweets = function () {
+     $.get('/tweets')
+     .then(response => {
+      renderTweets(response);
+     })
+   };
+
+   loadTweets();
 });
